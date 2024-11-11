d929ef7de8f64bbbebfde6108d7558c150b2c9af
Mateus Sousa <mateus.develp@gmail.com>
2024-11-11 13:24:01 -0300

    fix: ignore redis

--- app/common/cache_decorator.py
+++ app/common/cache_decorator.py
@@ -1,3 +1,4 @@
+import redis
 import json
 import functools
 from typing import Callable
@@ -18,15 +19,20 @@ def redis_cache(cache_key_prefix: str, ttl: int = 600) -> Callable:
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             cache_key = f"{cache_key_prefix}:{args[1].symbol.lower()}"
-            redis_client = RedisClient.get_client()
-
-            cached_data = redis_client.get(cache_key)
-            if cached_data:
-                return json.loads(cached_data)
-
+            try:
+                redis_client = RedisClient.get_client()
+                cached_data = redis_client.get(cache_key)
+                if cached_data:
+                    return json.loads(cached_data)
+            except (redis.ConnectionError, redis.TimeoutError) as e:
+                print(f"Redis connection error: {e}. Proceeding without cache.")
             result = func(*args, **kwargs)
+            try:
+                if redis_client:
+                    redis_client.setex(cache_key, ttl or settings.redis_cache_ttl, json.dumps(result))
+            except (redis.ConnectionError, redis.TimeoutError) as e:
+                print(f"Redis connection error on setting cache: {e}. Result not cached.")
 
-            redis_client.setex(cache_key, ttl or settings.redis_cache_tll, json.dumps(result))
             return result
 
         return wrapper
